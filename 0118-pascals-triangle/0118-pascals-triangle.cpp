712c1afe3bdc530a81c477aa2545fa5711d9995d
Aadikalra01 <creedismyname2@gmail.com>
2025-11-21 22:21:02 +0530

    Time: 0 ms (100%), Space: 9.8 MB (32.09%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -4,8 +4,7 @@ public:
           vector<vector<int>> ans(numRows);
 
         for (int i = 0; i < numRows; i++) {
-            ans[i].resize(i + 1);
-            ans[i][0] = ans[i][i] = 1;
+            ans[i] = vector<int>(i+1,1);
 
             for (int j = 1; j < i; j++) {
                 ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
